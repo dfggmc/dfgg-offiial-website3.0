2d812e63c03de9b869a14c998839c2d5f441cf95
XiaoFeng-QWQ <q1432777209@126.com>
2024-04-10 22:06:29 +0800

    删除无用代码

--- js/front.js
+++ js/front.js
@@ -56,34 +56,4 @@ $(function () {
     $('.navbar .navbar-toggler').on('click', function () {
         $(this).toggleClass('active');
     });
-
-    // ------------------------------------------------------ //
-    // For demo purposes, can be deleted
-    // ------------------------------------------------------ //
-
-    var stylesheet = $('link#theme-stylesheet');
-    $( "<link id='new-stylesheet' rel='stylesheet'>" ).insertAfter(stylesheet);
-    var alternateColour = $('link#new-stylesheet');
-
-    if ($.cookie("theme_csspath")) {
-        alternateColour.attr("href", $.cookie("theme_csspath"));
-    }
-
-    $("#colour").change(function () {
-
-        if ($(this).val() !== '') {
-
-            var theme_csspath = 'css/style.' + $(this).val() + '.css';
-
-            alternateColour.attr("href", theme_csspath);
-
-            $.cookie("theme_csspath", theme_csspath, { expires: 365, path: document.URL.substr(0, document.URL.lastIndexOf('/')) });
-
-        }
-
-        return false;
-    });
-
-
-
 });
